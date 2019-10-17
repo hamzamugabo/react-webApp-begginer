92811d5501751c5a3769849158ceac74e89e11e9
hamza mugabo <hamzamugabo3@gmail.com>
2019-10-18 02:27:08 +0300

    fix delete

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,7 +11,8 @@ class Counter extends Component {
         return(
         <div>
             <span  className={this.getBadgeClasses()}>{this.formatCounter()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary">Increment</button>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
         </div>
         ); 
     }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
-import Counter from './counter';
+import React, { Component } from "react";
+import Counter from "./counter";
 class Counters extends Component {
-    state = { 
-        counters:[
-            {id:1, value:4},
-            {id:2, value:0},
-            {id:3, value:0},
-            {id:4, value:0},
-
-        ]
-     }
-    render() { 
-        return (  
-            <div>
-                {this.state.counters.map(counter => <Counter key={counter.id} value={counter.value} />)}
-                
-                
-            </div>
-        );
-    }
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter
+            onDelete={this.handleDelete}
+            key={counter.id}
+            value={counter.value}
+            id={counter.id}
+            Counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Counters;
